1e522f5e763e9918ff0dd4a5857cfeec7261cd05
designernagesh <desginer.nagesh@gmail.com>
2023-06-11 16:28:58 +0530

    added smooth scroll

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -27,16 +27,16 @@ const Navbar = () => {
 
       <ul className="menu">
         <li>
-          <Link to="intro">Home</Link>
+          <Link to="intro" spy={true} smooth={true} duration={500}>Home</Link>
         </li>
         <li>
-          <Link to="features">Features</Link>
+          <Link to="features" spy={true} smooth={true} duration={500}>Features</Link>
         </li>
         <li>
-          <Link to="about">About</Link>
+          <Link to="about" spy={true} smooth={true} duration={500}>About</Link>
         </li>
         <li>
-          <Link to="contact">Contact</Link>
+          <Link to="contact" spy={true} smooth={true} duration={500}>Contact</Link>
         </li>
       </ul>
     </nav>
